2cbfe0623a9f2c9ed0da27874ac83b16fc22d167
FranciscoPedro06 <pedro2006francisco@gmail.com>
2025-05-26 15:10:42 -0300

    Add o construtor na classe User.java

--- app/src/main/java/com/reviva/app/models/User.java
+++ app/src/main/java/com/reviva/app/models/User.java
@@ -1,4 +1,13 @@
 package com.reviva.app.models;
 
 public class User {
+    private String uid;
+    private String name;
+    private String email;
+
+    public User(String uid, String name, String email) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+    }
 }
