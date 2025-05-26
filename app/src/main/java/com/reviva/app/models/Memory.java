8e77ed23be430b9c8d747bb62bcddbddaaf85587
Matheus Santana <mssantana@ecomp.uefs.br>
2025-05-26 20:23:11 -0300

    Add the constructor in class Memory

--- app/src/main/java/com/reviva/app/models/Memory.java
+++ app/src/main/java/com/reviva/app/models/Memory.java
@@ -1,4 +1,29 @@
 package com.reviva.app.models;
 
 public class Memory {
+    private String memory_id;
+    private String user_id;
+    private String title;
+    private String description;
+    private String media_url;
+    private String media_type;
+    private long createdAt;
+    private long unlockAt;
+    private boolean isUnlocked;
+
+    public Memory(String memory_id, String user_id, String title, String description, String media_url, String media_type, long createdAt, long unlockAt, boolean isUnlocked){
+        this.memory_id = memory_id;
+        this.user_id = user_id;
+        this.title = title;
+        this.description = description;
+        this.media_url = media_url;
+        this.media_type = media_type;
+        this.createdAt = createdAt;
+        this.unlockAt = unlockAt;
+        this.isUnlocked = isUnlocked;
+
+    }
+
 }
+
+
