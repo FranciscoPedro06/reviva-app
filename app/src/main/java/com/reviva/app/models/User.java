4b884db3f9a2c5f03bb05ae26a12e252b863bd20
Matheus Santana <mssantana@ecomp.uefs.br>
2025-05-26 20:23:29 -0300

    Update class User

--- app/src/main/java/com/reviva/app/models/User.java
+++ app/src/main/java/com/reviva/app/models/User.java
@@ -1,13 +1,17 @@
 package com.reviva.app.models;
 
+import java.util.UUID;
+
 public class User {
-    private String uid;
+    private String uId;
     private String name;
     private String email;
+    private String password;
 
-    public User(String uid, String name, String email) {
-        this.uid = uid;
+    public User(String uId, String name, String email, String password) {
+        this.uId = uId;
         this.name = name;
         this.email = email;
+        this.password = password;
     }
 }
