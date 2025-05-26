80624f0904993f341059dc5f0613b447acb00663
FranciscoPedro06 <pedro2006francisco@gmail.com>
2025-05-26 14:52:02 -0300

    Add classe User e Memory

---
+++ app/Memory.java
@@ -0,0 +1,10 @@
+public class Memory {
+    private String id;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private String userId;
+    private long unlockTimestamp;
+
+    // construtores, getters e setters
+}
---
+++ app/src/main/java/com/reviva/app/models/User.java
@@ -0,0 +1,4 @@
+package com.reviva.app.models;
+
+public class User {
+}
