c3d6e4b2061109e0660abbe49b400e848547539e
Matheus Santana <mssantana@ecomp.uefs.br>
2025-06-10 08:11:00 -0300

    Update file Model Memory

--- app/src/main/java/com/reviva/app/models/Memory.java
+++ app/src/main/java/com/reviva/app/models/Memory.java
@@ -1,88 +1,104 @@
 package com.reviva.app.models;
 
 public class Memory {
-    private String memory_id;
-    private String user_id;
+    private String memoryId;
+    private String userId;
     private String title;
     private String description;
-    private String media_url;
-    private String media_type;
+    private String mediaUrl;
+    private String mediaType;
     private long createdAt;
     private long unlockAt;
     private boolean isUnlocked;
 
-    // Constructor
-    public Memory(String memory_id, String user_id, String title, String description, String media_url, String media_type, long createdAt, long unlockAt, boolean isUnlocked){
-        this.memory_id = memory_id;
-        this.user_id = user_id;
+    // Constructs
+    public Memory() {}
+
+
+    public Memory(String memoryId, String userId, String title, String description,
+                  String mediaUrl, String mediaType, long createdAt, long unlockAt, boolean isUnlocked) {
+        this.memoryId = memoryId;
+        this.userId = userId;
         this.title = title;
         this.description = description;
-        this.media_url = media_url;
-        this.media_type = media_type;
+        this.mediaUrl = mediaUrl;
+        this.mediaType = mediaType;
         this.createdAt = createdAt;
         this.unlockAt = unlockAt;
         this.isUnlocked = isUnlocked;
-
     }
 
     // Getters
-    public String getMemory_id() {
-        return memory_id;
+    public String getMemoryId() {
+        return memoryId;
     }
 
-    public String getUser_id(){
-        return user_id;
+    public String getUserId() {
+        return userId;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
-    public String getMedia_url(){
-        return media_url;
+
+    public String getMediaUrl() {
+        return mediaUrl;
     }
 
-    public String getMedia_type(){
-        return media_type;
+    public String getMediaType() {
+        return mediaType;
     }
 
-    public long getCreatedAt(){
+    public long getCreatedAt() {
         return createdAt;
     }
 
-    public long getUnlockAt(){
+    public long getUnlockAt() {
         return unlockAt;
     }
 
-    public boolean getIsUnlocked(){
+    public boolean isUnlocked() {
         return isUnlocked;
     }
 
     // Setters
-    public String setTitle(String title){
-        return title;
+    public void setMemoryId(String memoryId) {
+        this.memoryId = memoryId;
     }
-    public String setDescription(String description){
-        return description;
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
-    public String setMedia_url(String media_url){
-        return media_url;
+
+    public void setTitle(String title) {
+        this.title = title;
     }
-    public String setMedia_type(String media_type) {
-        return media_type;
+
+    public void setDescription(String description) {
+        this.description = description;
     }
-    public long setCreatedAt(long createdAt){
-        return createdAt;
+
+    public void setMediaUrl(String mediaUrl) {
+        this.mediaUrl = mediaUrl;
     }
-    public long setUnlockAt(long unlockAt){
-        return unlockAt;
-        }
-    public boolean setIsUnlocked(boolean isUnlocked){
-        return isUnlocked;
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUnlockAt(long unlockAt) {
+        this.unlockAt = unlockAt;
+    }
+
+    public void setUnlocked(boolean unlocked) {
+        isUnlocked = unlocked;
     }
 }
-
-
