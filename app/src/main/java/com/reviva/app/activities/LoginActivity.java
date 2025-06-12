8130195ad83b27d59772f47d9050c45ec9b0fc80
Matheus Santana <mssantana@ecomp.uefs.br>
2025-06-12 14:11:54 -0300

    Update files (refs #2)

--- app/src/main/java/com/reviva/app/activities/LoginActivity.java
+++ app/src/main/java/com/reviva/app/activities/LoginActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.reviva.app.R;
@@ -21,18 +23,21 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnEntrar;
     private TextView cadastrarText;
     private FirebaseAuth mAuth;
+    private ImageView google;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         mAuth = FirebaseAuth.getInstance();
-
         emailEditText = findViewById(R.id.email);
         senhaEditText = findViewById(R.id.senha);
-        btnEntrar = findViewById(R.id.btnEntar);
+        btnEntrar = findViewById(R.id.btnEntrar);
         cadastrarText = findViewById(R.id.cadastrar);
+        google = findViewById(R.id.google);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +62,14 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(LoginActivity.this, "Erro: email ou senha incorretos.", Toast.LENGTH_SHORT).show();
                             }
                         });
+            }
+        });
+
+        // Ação do botão Google
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Login com Google", Toast.LENGTH_SHORT).show();
             }
         });
 
