ac222c00980be6887cd329cfbd3998c12adcc766
Matheus Santana <mssantana@ecomp.uefs.br>
2025-06-16 22:31:59 -0300

    Update files (refs #4)

--- app/src/main/java/com/reviva/app/activities/InicialActivity.java
+++ app/src/main/java/com/reviva/app/activities/InicialActivity.java
@@ -1,45 +1,41 @@
-package com.reviva.app.activities; // Ajuste o pacote conforme seu projeto
+package com.reviva.app.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-// Certifique-se de que o nome da classe está correto, ex: InicialActivity
+import com.reviva.app.R;
+
+
+
 public class InicialActivity extends AppCompatActivity {
 
-    private Button btnCadastrar;
-    private Button btnLogin;
+    private Button btnComecar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicial); // Verifique se o nome do layout está correto
+        setContentView(R.layout.activity_telainicio);
 
-        // Inicializar Views
-        btnCadastrar = findViewById(R.id.btnCadastrar);
-        btnLogin = findViewById(R.id.btnLogin);
 
-        // Configurar OnClickListener para o botão Cadastrar
-        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+        btnComecar = findViewById(R.id.btnComecar);
+
+
+
+        btnComecar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Cria um Intent para iniciar a Activity de Cadastro
                 Intent intent = new Intent(InicialActivity.this, CadastroActivity.class);
                 startActivity(intent);
             }
         });
+    }
 
-        // Configurar OnClickListener para o botão Login
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Cria um Intent para iniciar a Activity de Login
-                Intent intent = new Intent(InicialActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onBackPressed() {
+        // Não faz nada, bloqueando o botão Voltar
     }
 }
